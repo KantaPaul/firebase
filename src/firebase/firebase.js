f3ecfcffd8efffaf02d038801a857f2f4308ce5e
KantaPaul <pobon.paul1994@gmai.com>
2018-08-14 23:48:55 +0600

    firebase file add push method

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,6 +15,80 @@ firebase.initializeApp(config);
 
 let database = firebase.database();
 
+// database.ref('expenses').once('value')
+//   .then((snapshot) => {
+//     let val = snapshot.val();
+//     console.log(val);
+//     let expense = [];
+//     snapshot.forEach((childShort) => {
+//       expense.push({
+//         id: childShort.key,
+//         ...childShort
+//       });
+//     })
+//     console.log(expense);
+//   }).catch((error) => {
+//     console.log(error)
+//   })
+
+database.ref('expenses').on('value', (snapshot) => {
+  let expense = [];
+    snapshot.forEach((childShort) => {
+      expense.push({
+        id: childShort.key,
+        ...childShort
+      });
+    })
+    console.log(expense);
+})
+
+database.ref('expenses').on('child_removed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val())
+})
+
+database.ref('expenses').on('child_changed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val())
+})
+
+database.ref('expenses').on('child_added', (snapshot) => {
+  console.log(snapshot.key, snapshot.val())
+})
+
+// database.ref('expenses').push({
+//   description: 'Rent bill',
+//   note: '',
+//   amount: 456545,
+//   createdAt: 5654646
+// });
+
+// database.ref('expenses').push({
+//   description: 'Gas bill',
+//   note: '',
+//   amount: 6545,
+//   createdAt: 565894646
+// });
+
+// database.ref('expenses').push({
+//   description: 'Water bill',
+//   note: '',
+//   amount: 1654,
+//   createdAt: 65879
+// });
+
+database.ref('expenses').push({
+  description: 'Phone bill',
+  note: '',
+  amount: 1654,
+  createdAt: 65879
+});
+
+// database.ref('notes/-LJt0zkTx3FTbm7eSvZa').remove();
+
+// database.ref('notes').push({
+//   title: 'To do',
+//   body: 'Go for run'
+// });
+
 // database.ref().once('value').then((snapshot) => {
 //   let val = snapshot.val();
 //   console.log(val)
@@ -22,17 +96,17 @@ let database = firebase.database();
 //   console.log(e)
 // })
 
-let onvaluChange = database.ref().on('value', (snapshot) => { 
-  let val = snapshot.val();
-  console.log(val)
-}, (e) => {
-  console.log(e)
-})
+// let onvaluChange = database.ref().on('value', (snapshot) => { 
+//   let val = snapshot.val();
+//   console.log(val)
+// }, (e) => {
+//   console.log(e)
+// })
 
-database.ref().on('value', (snapshot) => {
-  let val = snapshot.val();
-  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
-})
+// database.ref().on('value', (snapshot) => {
+//   let val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+// })
 
 // setTimeout(() => {
 //   database.ref('age').set(45)
@@ -101,3 +175,34 @@ database.ref().on('value', (snapshot) => {
 //   stressLabel: 6,
 //   'location/city': 'Jhenidah'
 // })
+
+// let firebaseNotes = {
+//   notes: {
+//     iasdasfsdfdsf: {
+//       title: 'First Note!',
+//       body: 'This is my note'
+//     },
+//     asdasd454f: {
+//       title: 'Another Note!',
+//       body: 'This is another note'
+//     }
+//   }
+// }
+
+// let notes = [
+//   {
+//     id: '12',
+//     title: 'First Note!',
+//     body: 'This is my note'
+//   },
+//   {
+//     id: '13',
+//     title: 'Another Note!',
+//     body: 'This is another note'
+//   }
+// ]
+
+// console.log(notes)
+
+// database.ref('notes').set(notes)
+// database.ref('notes').set(notes)
